27209a4c83f564f2e470b047eac16a04a741e9a7
Oksana Zalivan <ksyushamartinovska@gmail.com>
2025-10-02 21:36:52 +0300

    add module-1-1

---
+++ js/task.js
@@ -0,0 +1,45 @@
+/* Завдання 1 */
+const myName = "Oksana";
+console.log(myName);
+
+/* Завдання 2 */
+const age = 37;
+const message = `Мені ${age} років`;
+console.log(message);
+
+/* Завдання 3 */
+const x = 9;
+const y = 3;
+const result1 = x + y;
+const result2 = x - y;
+const result3 = x * y;
+const result4 = x / y;
+const result5 = x % y;
+const result6 = x + y ** 2;
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+console.log(result6);
+
+/* Задання 4 */
+const city = "Korosten";
+const message2 = `Я живу в місті ${city}`;
+console.log(message2);
+
+/* Завдання 5 */
+const firstName = "ksenya";
+const lastName = "martinovska";
+const emailDomen = "@gmail.com";
+const fullName = firstName + lastName;
+const myEmail = fullName + emailDomen;
+
+console.log(myEmail);
+
+/* Завдання 6 */
+const price = 500;
+const message3 = "Ціна: " + price + " грн"
+
+console.log(message3);
